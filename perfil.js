6f1fff4896b31bf68543ef2051ebd1cd2f9b2458
apenasliana <miresukali@gmail.com>
2022-07-24 20:30:04 -0300

    git hub pages fix

--- perfil.js
+++ perfil.js
@@ -1,5 +1,5 @@
 function deslogar(){
-    window.location.href = "/";
+    window.location.href = "/DTI-login-page/";
 }
 
 const queryString = window.location.search;
@@ -8,7 +8,8 @@ const urlParams = new URLSearchParams(queryString)
 fetch("validator.json").then(res=>{
     res.json().then(data => {
         if (urlParams.get('token') != data.token) {
-            window.location.href = "/";
+            // window.location.replace("/DTI-login-page/")
+            console.log(data)
         }
     })
 })
